703df3b44d2370fd8beaf6963b379baf6af63f4d
felipeclarindo <fglpc2@gmail.com>
2025-03-30 15:22:19 -0300

    feat(app): Started the basic structure of the pages.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,15 @@
+import { NavBar } from "@/components/NavBar";
+
 export default function DashboardPage() {
-  return <></>;
+  return (
+    <>
+      <NavBar active="Dashboard" />
+
+      <main className="flex justify-center">
+        <div className="bg-gray-200 m-4 p-4 rounded min-w-1/3">
+          <h2>Dashboard</h2>
+        </div>
+      </main>
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { NavBar } from "@/components/NavBar";
+
 export default function HomePage() {
-  return <></>;
+  return (
+    <>
+      <NavBar active="Home" />
+
+      <main className="flex justify-center">
+        <div className="bg-gray-200 m-4 p-4 rounded min-w-1/3">
+          <h2>Home</h2>
+        </div>
+      </main>
+    </>
+  );
 }
