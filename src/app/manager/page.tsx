bc7d5d5b1650f324355190e65f8deb6fe873fa82
felipeclarindo <fglpc2@gmail.com>
2025-04-08 02:05:25 -0300

    feat(application): Adding fix to the errors and make the review of the code.

--- src/app/manager/page.tsx
+++ src/app/manager/page.tsx
@@ -1,40 +1,57 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { CustomButton } from "@/components/CustomButton";
+import { LoadingSpinner } from "@/components/LoadingSpinner";
 import { NavBar } from "@/components/NavBar";
+import { StudentItem } from "@/components/StudentItem";
+import { ErrorNotification } from "@/components/ui/ErrorNotification";
+import { AddModal } from "@/components/ui/modals/AddModal";
+import { SuccessNotification } from "@/components/ui/SuccessNotification";
 import { StudentProps } from "@/interfaces";
-import { useEffect, useState } from "react";
+import { useStudents } from "@/actions/students-actions";
 
 export default function ManagerPage() {
-  const [students, setStudents] = useState<StudentProps[]>([]);
-
-  const fetchStudents = async () => {
-    try {
-      const response = await fetch("http://localhost:8080/api/students", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const result = await response.json();
-      setStudents(result);
-    } catch (error) {
-      console.error("Failed to fetch students:", error);
-    }
-  };
+  const { students, loading, error, setError, fetchStudents } = useStudents();
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [successVisible, setSuccessVisible] = useState(false);
+  const [selectedStudent, setSelectedStudent] = useState<StudentProps | null>(
+    null
+  );
+  const [isNotesModalOpen, setIsNotesModalOpen] = useState(false);
 
   useEffect(() => {
     fetchStudents();
-  }, []);
+  }, [fetchStudents]);
+
+  const handleViewNotes = (student: StudentProps) => {
+    setSelectedStudent(student);
+    setIsNotesModalOpen(true);
+  };
+  
+  const handleAddClick = () => {
+    setIsAddModalOpen(true);
+  };
+
+  const handleStudentAdded = async () => {
+    await fetchStudents();
+    setIsAddModalOpen(false);
+    setShowSuccess(true);
+    setSuccessVisible(true);
+
+    setTimeout(() => {
+      setSuccessVisible(false);
+      setTimeout(() => {
+        setShowSuccess(false);
+      }, 500);
+    }, 2000);
+  };
 
   return (
     <>
       <NavBar active="Manager" />
-
-      <main className="flex justify-center items-center min-h-screen">
+      <main className="flex justify-center items-center min-h-[70vh]">
         <div className="flex-col items-center justify-center p-10 bg-gray-200 m-4 rounded min-w-[70vw] shadow-lg">
           <div className="flex items-start mb-4">
             <div className="flex-col mr-8 min-w-[220px] p-4">
@@ -43,77 +60,86 @@ export default function ManagerPage() {
               </h2>
               <hr className="border-t-2 border-black" />
               <div className="flex flex-col mt-4 space-y-2 gap-1">
-                <CustomButton text="Add" />
-                <CustomButton text="Update" />
-                <CustomButton text="Delete" />
+                <CustomButton text="Add" onClick={handleAddClick} />
               </div>
             </div>
-
             <div className="flex-grow">
               <div className="border border-gray-400 rounded">
                 <table className="min-w-full border-collapse">
                   <thead className="bg-gray-300">
                     <tr>
-                      <td
+                      <th
                         colSpan={6}
                         className="border px-4 py-2 text-center font-bold text-lg"
                       >
-                        Students Manager
-                      </td>
+                        Students Registers
+                      </th>
                     </tr>
                     <tr>
                       <th className="border px-4 py-2 text-left">ID</th>
                       <th className="border px-4 py-2 text-left">Name</th>
                       <th className="border px-4 py-2 text-left">Course</th>
-                      <th className="border px-4 py-2 text-left">Grade</th>
                       <th className="border px-4 py-2 text-left">Period</th>
+                      <th className="border px-4 py-2 text-left">Notes</th>
                       <th className="border px-4 py-2 text-left">Action</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {students.map((student) => (
-                      <tr key={student.id}>
-                        <td className="border px-4 py-2 text-center">
-                          {student.id}
-                        </td>
-                        <td className="border px-4 py-2 text-center">
-                          {student.name}
-                        </td>
-                        <td className="border px-4 py-2 text-center">
-                          {student.course}
-                        </td>
-                        <td className="border px-4 py-2 text-center">
-                          {student.grade}
-                        </td>
-                        <td className="border px-4 py-2 text-center">
-                          {student.period}
-                        </td>
-                        <td className="border px-4 py-2 max-w-30 text-center">
-                          <CustomButton text="Edit" /> {" | "}
-                          <CustomButton text="Delete" />
-                        </td>
-                      </tr>
-                    ))}
-                    {Array.from({ length: 7 }).map((_, index) => (
-                      <tr key={`empty-row-${index}`}>
-                        <td className="border px-4 py-2 ">&nbsp;</td>
-                        <td className="border px-4 py-2">&nbsp;</td>
-                        <td className="border px-4 py-2">&nbsp;</td>
-                        <td className="border px-4 py-2">&nbsp;</td>
-                        <td className="border px-4 py-2">&nbsp;</td>
-                        <td className="border px-4 py-2 max-w-30 text-center">
-                          <CustomButton text="Edit" /> {" | "}
-                          <CustomButton text="Delete" />
-                        </td>
-                      </tr>
-                    ))}
+                    {(() => {
+                      if (loading) {
+                        return (
+                          <tr>
+                            <td colSpan={6} className="text-center py-4">
+                              <LoadingSpinner />
+                            </td>
+                          </tr>
+                        );
+                      }
+                      if (students && students.length > 0) {
+                        return students.map((student: StudentProps) => (
+                          <StudentItem key={student.id} {...student} />
+                        ));
+                      }
+                      return (
+                        <tr>
+                          <td colSpan={6} className="text-center px-4 py-2">
+                            No students found.
+                          </td>
+                        </tr>
+                      );
+                    })()}
                   </tbody>
                 </table>
+              </div>
+              <div className="mt-4 ml-2">
+                {error && (
+                  <ErrorNotification
+                    addClass="ml-4"
+                    message={error}
+                    onClose={() => setError(null)}
+                  />
+                )}
+                {showSuccess && (
+                  <SuccessNotification
+                    addClass={`ml-4 transition-opacity duration-500 ${
+                      successVisible ? "opacity-100" : "opacity-0"
+                    }`}
+                    message="Student added successfully!"
+                    onClose={() => setShowSuccess(false)}
+                  />
+                )}
               </div>
             </div>
           </div>
         </div>
       </main>
+      {isAddModalOpen && (
+        <AddModal
+          isOpenModal={isAddModalOpen}
+          onClose={() => setIsAddModalOpen(false)}
+          onSuccess={handleStudentAdded}
+        />
+      )}
     </>
   );
 }
